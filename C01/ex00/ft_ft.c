6cf34b5d2658c50d0aad90875cc7ee39c9e0bcec
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-02-28 18:32:43 -0300

    C01

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 11:32:51 by mariafer          #+#    #+#             */
+/*   Updated: 2026/02/28 12:11:49 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*#include <stdio.h>
+int	main(void)
+{
+	int*	pnbr;
+	int	number;
+
+	number = 0;
+	printf ("Valor anterior do nbr: %d\n", number);
+	pnbr = &number;
+	ft_ft(pnbr);
+	printf ("Valor posterior do nbr: %d", number);
+	return (0);
+}*/
