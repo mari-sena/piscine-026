5d44164af44cdf14b4c71fbbb99f2d6fe65598f4
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-02-28 18:45:03 -0300

    Correct reverse logic and update main functionedit 7
    Fix the logic for reversing the integer array and update the main function to use a static array.

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 15:14:33 by mariafer          #+#    #+#             */
-/*   Updated: 2026/02/27 15:23:47 by mariafer         ###   ########.fr       */
+/*   Updated: 2026/02/28 18:44:14 by mariafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,20 +16,19 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	aux;
 
 	aux = size;
-	while (aux >= size)
+	while (aux <= size || aux == 0)
 	{
 		printf("%d", tab[aux]);
 		//*tab[aux] = tab[aux];
 		aux--;
 	}	
 }
-
-void	main(void)
+int	main(void)
 {
-	int	*tab;
-	int	size;
+	int	arr[6] = {1, 2, 3, 4, 5, 6};
+	int	*tab = arr;
+	int	size = 6;
 
-	size = 6;
-	tab[size] = [1, 2, 3, 4, 5, 6];
 	ft_rev_int_tab(tab, size);
+	return (0);
 }
