3efea658362dd4b628d631610d2d3969f228b91f
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-01 12:20:08 -0300

    Refactor array reversal function
    Refactor ft_rev_int_tab function to reverse an integer array in place. Update main function to demonstrate the reversal.

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -5,30 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/27 15:14:33 by mariafer          #+#    #+#             */
-/*   Updated: 2026/02/28 18:44:14 by mariafer         ###   ########.fr       */
+/*   Created: 2026/03/01 11:18:27 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/01 11:18:38 by mariafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	aux;
+	int		ref_beggning;
+	int		ref_last;
 
-	aux = size;
-	while (aux <= size || aux == 0)
+	ref_beggning = 0;
+	ref_last = size - 1;
+	while (ref_beggning <= size - 1)
 	{
-		printf("%d", tab[aux]);
-		//*tab[aux] = tab[aux];
-		aux--;
-	}	
+		tab[ref_beggning] = tab[ref_last];
+		ref_beggning++;
+		ref_last--;
+	}
+	// ref_beggning = 0;
+	// while (ref_beggning <= size - 1)
+	// {
+	// 	tab[ref_beggning] = arr_ref_beggning[ref_beggning];
+	// 	ref_beggning++;
+	// 	ref_last--;
+	// }
 }
-int	main(void)
-{
-	int	arr[6] = {1, 2, 3, 4, 5, 6};
-	int	*tab = arr;
-	int	size = 6;
 
-	ft_rev_int_tab(tab, size);
-	return (0);
+int main(void)
+{
+	int	size = 6;
+	int num[] = {1, 2, 3, 4, 5, 6};
+
+    printf("%d\n", num[0]);
+    printf("%d\n", num[1]);
+    printf("%d\n", num[2]);
+    printf("%d\n", num[3]);
+    printf("%d\n", num[4]);
+    printf("%d\n\n", num[5]);
+    ft_rev_int_tab(num, size);
+    printf("%d\n", num[0]);
+    printf("%d\n", num[1]);
+	printf("%d\n", num[2]);
+	printf("%d\n", num[3]);
+    printf("%d\n", num[4]);
+    printf("%d\n", num[5]);
+    return (0);
 }
