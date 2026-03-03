4ff262f400d73a75278627a1e573cc827738e91e
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-03 10:30:25 -0300

    Update ft_str_is_alpha.c

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -15,24 +15,30 @@ int	ft_str_is_alpha(char *str)
 	int	aux;
 
 	aux = 0;
+	if (str[aux] == '\0')
+		return (1);
 	while (str[aux] != '\0')
 	{
-		if ((str[aux] < 'A' && str[aux] > 'Z')
-			&& (str[aux] < 'a' && str[aux] > 'z')
-			&& str[aux] != 32)
-		{
+		if ((str[aux] >= 'A' && str[aux] <= 'Z')
+			|| (str[aux] >= 'a' && str[aux] <= 'z'))
+			aux++;
+		else
 			return (0);
-		}
-		aux++;
 	}
 	return (1);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	char	str[] = "as 4dfsadkjf";
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	str[] = "";
+// 	char	str1[] = " FASFSD";
+// 	char	str2[] = "as 4dfsadkjf";
+// 	char	str3[] = "adbcefghijkl";
 
-	printf("%d", ft_str_is_alpha(str));
-	return (0);
-}
+// 	printf("%d\n", ft_str_is_alpha(str));
+// 	printf("%d\n", ft_str_is_alpha(str1));
+// 	printf("%d\n", ft_str_is_alpha(str2));
+// 	printf("%d\n", ft_str_is_alpha(str3));
+// 	return (0);
+// }
