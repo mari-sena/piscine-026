b21e66f77de2ad1d1efc65d4d0aa5c068ea7a5f7
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-05 11:51:32 -0300

    ex03 com nome certo

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft__str_is_numeric.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 11:20:41 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/02 13:40:03 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *src)
+{
+	int	aux;
+
+	aux = 0;
+	while (src[aux] != '\0')
+	{
+		if (src[aux] >= '0' && src[aux] <= '9')
+			aux++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	src[] = "123";
+// 	char	src2[] = "123a";
+// 	char	src3[] = " 123a";
+
+// 	printf("%d\n", ft_str_is_numeric(src));
+// 	printf("%d\n", ft_str_is_numeric(src2));
+// 	printf("%d\n", ft_str_is_numeric(src3));
+// 	return(0);
+// }
