9c53942c35b9109d1d1d3df57841d65213e03f93
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-08 20:34:22 -0300

    Update ft_strcat.c

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -1,4 +1,16 @@
-char    *ft_strcat(char *dest, char *src)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 11:33:51 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/08 11:33:54 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src)
 {
 	int	aux_dest;
 	int	aux_src;
@@ -7,7 +19,6 @@ char    *ft_strcat(char *dest, char *src)
 	aux_src = 0;
 	while (dest[aux_dest] != '\0')
 	{
-		dest[aux_dest] = dest[aux_dest];
 		aux_dest++;
 	}
 	while (src[aux_src] != '\0')
@@ -16,6 +27,7 @@ char    *ft_strcat(char *dest, char *src)
 		aux_src++;
 		aux_dest++;
 	}
+	dest[aux_dest] = '\0';
 	return (dest);
 }
 
@@ -23,12 +35,12 @@ char    *ft_strcat(char *dest, char *src)
 // #include <string.h>
 // int main(void)
 // {
-//     char	dest[] = "Hello ";
+//     char	dest[20] = "Hello ";
 //     char	src[] = "World!";
-//     char	dest2[] = "Hello ";
-//     char	src2[] = "World!";
+// 	char	dest2[20] = "Hello ";
+// 	char	src2[] = "World!";
 
 //     printf("%s\n", ft_strcat(dest, src));
-//     printf("%s", strcat(dest2, src2));
+//     printf("%s\n", strcat(dest2, src2));
 //     return (0);
 // }
