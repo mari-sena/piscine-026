b6cb96e4788c2ccd41ba44fd8e75095cc3293dad
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-08 15:26:37 -0300

    putnbr finished

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -9,39 +9,57 @@
 /*   Updated: 2026/03/07 11:27:15 by mariafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putnbr(int nb)
 {
 	char	aux;
-	char	decimal;
-	
-	if (nb < 10)
+	long	num;
+
+	num = nb;
+	if (num < 0)
 	{
-		aux = '0' + nb;
-		write(1, &aux, 1);
+		write(1, "-", 1);
+		num = -num;
 	}
-	else if (nb < 100)
-	{
-		aux = nb % 10;
-		decimal = nb / 10;
-		aux += '0';
-		decimal += '0';
-		write(1, &decimal, 1);
-		write(1, &aux, 1);
-	}
+	if (num >= 10)
+		ft_putnbr(num / 10);
+	aux = (num % 10) + '0';
+	write(1, &aux, 1);
 }
 
-int	main(void)
-{
-	ft_putnbr(2);
-	write(1, "\n", 1);
-	write(1, "\n", 1);
-	ft_putnbr(42);
-	write(1, "\n", 1);
-	// ft_putnbr(142);
-	// write(1, "\n", 1);
-	// ft_putnbr(1042);
-	// write(1, "\n", 1);
-	// ft_putnbr(-2147483648);
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	ft_putnbr(-42);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-2);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(0);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(2);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(42);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(142);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(1042);
+// 	write(1, " (1.042)\n", 10);
+// 	ft_putnbr(10142);
+// 	write(1, " (10.142)\n", 11);
+// 	ft_putnbr(100142);
+// 	write(1, " (100.142)\n", 12);
+// 	ft_putnbr(1100142);
+// 	write(1, " (1.100.142)\n", 14);
+// 	ft_putnbr(10100142);
+// 	write(1, " (10.100.142)\n", 15);
+// 	ft_putnbr(142100142);
+// 	write(1, " (142.100.142)\n", 16);
+// 	ft_putnbr(1142100142);
+// 	write(1, " (1.142.100.142)\n", 17);
+// 	ft_putnbr(2147483647);
+// 	write(1, " (2.147.483.647)\n", 17);
+// 	ft_putnbr(-2147483648);
+// 	write(1, " (-2.147.483.648)\n", 18);
+// }
