42dbad31ffe0d282277ac9956c5cb2618d8a092f
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-09 17:53:49 -0300

    continuando o atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,35 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/08 21:42:37 by marianasena       #+#    #+#             */
-/*   Updated: 2026/03/08 21:42:40 by marianasena      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-int ft_atoi(char *str)
-{
-    int index;
-
-    index = 0;
-    while (str[index])
-    {
-        if (str[index] == '+' || str[index] == '-')
-        if (str[index] >= '0' && str[index] <= '9')
-            str[index] += 0;
-        str++;
-        index++;
-    }
-}
 
 #include <stdio.h>
-int main(int argc, char **argv)
+
+int	ft_atoi(char *str)
 {
-    if (argc != 2)
-        return (0);
-    printf("%d", ft_atoi(argv[1]));
-    return (0);
+	int	index;
+	int	signal_counter;
+	int	result;
+
+	index = 0;
+	result = 0;
+	signal_counter = 0;
+	while (str[index])
+	{
+		// if (str[index] == '\f'
+		// 	|| str[index] == '\n'
+		// 	|| str[index] == '\r'
+		// 	|| str[index] == '\t'
+		// 	|| str[index] == ' ')
+		if (str[index] == '-')
+			signal_counter++;
+		if (str[index] >= '0' && str[index] <= '9')
+		{
+			result *= 10;
+			result += (int)str[index] - 48;
+		}
+		str++;
+		index++;
+	}
+	return (result);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (0);
+	printf("%d", ft_atoi(argv[1]));
+	return (0);
 }
