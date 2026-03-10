9c677ccdd32a03b1456f437ec8f131d6b57cd85b
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-10 17:35:33 -0300

    finished atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,5 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 15:25:56 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/08 15:25:57 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdio.h>
+
+int	ft_handle_spaces(int index, char *str)
+{
+	while (str[index] == ' '
+		|| str[index] == '\n'
+		|| str[index] == '\f'
+		|| str[index] == '\r'
+		|| str[index] == '\t')
+		index++;
+	return (index);
+}
+
+int	ft_handle_signs(int *signal_counter, int index, char *str)
+{
+	while (str[index] == '-'
+		|| str[index] == '+')
+	{
+		if (str[index] == '-')
+			*signal_counter = *signal_counter + 1;
+		index++;
+	}
+	return (index);
+}
+
+int	ft_handle_numbers(int result, int index, char *str)
+{
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		result *= 10;
+		result += (int)str[index] - 48;
+		index++;
+	}
+	return (result);
+}
 
 int	ft_atoi(char *str)
 {
@@ -10,30 +55,18 @@ int	ft_atoi(char *str)
 	index = 0;
 	result = 0;
 	signal_counter = 0;
-	while (str[index])
-	{
-		// if (str[index] == '\f'
-		// 	|| str[index] == '\n'
-		// 	|| str[index] == '\r'
-		// 	|| str[index] == '\t'
-		// 	|| str[index] == ' ')
-		if (str[index] == '-')
-			signal_counter++;
-		if (str[index] >= '0' && str[index] <= '9')
-		{
-			result *= 10;
-			result += (int)str[index] - 48;
-		}
-		str++;
-		index++;
-	}
+	index = ft_handle_spaces(index, str);
+	index = ft_handle_signs(&signal_counter, index, str);
+	result = ft_handle_numbers(result, index, str);
+	if (signal_counter % 2)
+		result *= -1;
 	return (result);
 }
 
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (0);
-	printf("%d", ft_atoi(argv[1]));
-	return (0);
-}
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 		return (0);
+// 	printf("%d", ft_atoi(argv[1]));
+// 	return (0);
+// }
