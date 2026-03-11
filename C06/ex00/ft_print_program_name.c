f62482021677531906f0e4d478c0a9f49aab28e5
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-11 18:03:52 -0300

    Update ft_print_program_name.c

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 18:02:40 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/11 18:02:41 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_print_program_name(char *arg)
+void	ft_print_program_name(char *arg)
 {
-    int index;
+	int	index;
 
-    index = 0;
-    while (arg[index] != '\0')
-    {
-        write(1, &arg[index], 1);
-        index++;
-    }
-    write(1, "\n", 1);
+	index = 0;
+	while (arg[index] != '\0')
+	{
+		write (1, &arg[index], 1);
+		index++;
+	}
+	write (1, "\n", 1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    if (argc == 1)
-        ft_print_program_name(argv[0]);
-    return (0);
+	if (argc == 1)
+		ft_print_program_name(argv[0]);
+	return (0);
 }
