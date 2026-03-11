a1dd40da297d0a07ae1d77119c97fc2c63c30618
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-11 14:59:24 -0300

    ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 11:53:31 by mariafer          #+#    #+#             */
+/*   Updated: 2026/03/11 11:53:33 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	int	next;
+	int	result;
+	int	temp;
+
+	result = 1;
+	next = 0;
+	temp = 0;
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	while (index >= 0)
+	{
+		temp = result;
+		result = temp + next;
+		next = temp;
+		index--;
+	}
+	return (temp);
+}
+
+// #include <stdio.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	index;
+
+// 	index = 5;
+// 	// ft_fibonacci(index);
+// 	printf("%d\n", ft_fibonacci(index));
+// 	return (0);
+// }
