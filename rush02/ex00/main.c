20932ab0d9be1c34fa4f5a1b90ece17d7d3a12dc
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-15 10:29:01 -0300

    Refactor dictionary sanitization and memory handling
    Refactor and improve the dictionary sanitization process, including memory allocation adjustments and validation checks.

--- rush02/ex00/main.c
+++ rush02/ex00/main.c
@@ -11,50 +11,6 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-
-int		ft_strlen(char *str);
-
-void	ft_def_args(char **nb, int argc, char *argv[], char **dict_name);
-
-int		ft_is_valid_number(char *nb);
-
-void	ft_read_dict(char *dict_name);
-
-int	ft_baby_atoi(char *str)
-{
-	int	index;
-	int	result;
-
-	index = 0;
-	result = 0;
-	while (str[index] >= '0' && str[index] <= '9')
-	{
-		result *= 10;
-		result += (int)str[index] - 48;
-		index++;
-	}
-	return (result);
-}
-
-char	*ft_str_range(char *start, char *end)
-{
-	int		index;
-	char	*dest;
-
-	dest = (char *)malloc((end - start) + 1);
-	if (!dest)
-		return (NULL);
-	index = 0;
-	while (start < end)
-	{
-		dest[index] = *start;
-		start++;
-		index++;
-	}
-	dest[index] = '\0';
-	return (dest);
-}
 
 struct s_dict
 {
@@ -62,58 +18,84 @@ struct s_dict
 	char	*translation;
 };
 
-#include <stdio.h>
-void	ft_dict_sanitize(char *dict_values)
+int		ft_is_space(char c);
+int		ft_strlen(char *str);
+int		ft_baby_atoi(char *str);
+int		ft_is_valid_number(char *nb);
+int		ft_read_dict(char *dict_name, char *dict_values, int size);
+void	ft_def_args(char **nb, int argc, char *argv[], char **dict_name);
+char	*ft_str_range(char *start, char *end);
+
+int	ft_dict_sanitize(char *dict_values, struct s_dict *translations)
 {
-	int				index;
-	int				index_dict;
-	int				index_start;
-	struct s_dict	translations[5000]; // ARRUME MEU TAMANHO
+	int	index;
+	int	index_dict;
+	int	index_start;
 
 	index = 0;
 	index_dict = 0;
 	index_start = 0;
 	while (dict_values[index] != '\0')
 	{
-		if (dict_values[index] != ' ' || dict_values[index] != '\n')
+		while (ft_is_space(dict_values[index]))
+			index++;
+		if (dict_values[index] != '\0')
 		{
 			index_start = index;
 			while ((dict_values[index] >= '0')
 				&& (dict_values[index] <= '9'))
 				index++;
+			// CORRIGIR: \/ Se vier letra no index isso vai retornar 0
 			translations[index_dict].key
 					= ft_baby_atoi(&dict_values[index_start]);
-			if (dict_values[index] == ':' && dict_values[index] == ' ')
+			while (ft_is_space(dict_values[index]))
+				index++;
+			if (dict_values[index] == ':')
+				index++;
+			while (ft_is_space(dict_values[index]))
 				index++;
 			index_start = index;
-			while (dict_values[index] != '\n' || dict_values[index] != '\0')
+			while (dict_values[index] != '\n' && dict_values[index] != '\0')
 				index++;
 			translations[index_dict].translation =
 				ft_str_range(&dict_values[index_start], &dict_values[index]);
 			index_dict++;
 		}
-		index++;
-		printf("%d\n", translations[1].key);
-		printf("%s\n", translations[0].translation);
 	}
+	return (index_dict);
 }
 
+#include <stdio.h>
+#include <stdlib.h>
 int	main(int argc, char *argv[])
 {
-	char	*nb;
-	char	*dict_name;
-	char	dict_values[695];
+	char			*nb;
+	char			*dict_name;
+	char			dict_values[5000]; // ARRUME MEU TAMANHO
+	struct s_dict	translations[5000]; // ARRUME MEU TAMANHO
+	int	count_struct;
+	int	index_struct;
 
 	if (argc == 1)
 	{
-		write(1, "Error", 5);
+		write(1, "Error\n", 6);
 		return (0);
 	}
 	dict_name = "numbers.dict";
 	ft_def_args(&nb, argc, argv, &dict_name);
 	if (ft_is_valid_number(nb))
 		return (0);
-	ft_read_dict(dict_name);
-	ft_dict_sanitize(dict_values);
+	if (ft_read_dict(dict_name, dict_values, 5000)) // ARRUME MEU TAMANHO
+		return (0);
+	count_struct = ft_dict_sanitize(dict_values, translations);
+	index_struct = 0;
+	while (index_struct < count_struct)
+	{
+		printf("%d -> %s\n",
+			translations[index_struct].key,
+			translations[index_struct].translation);
+		free(translations[index_struct].translation);
+		index_struct++;
+	}
 	return (0);
 }
