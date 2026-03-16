4ab8e249eec60c1037cfc89020c0c43e1f5cb4df
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-16 13:01:25 -0300

    Update ft_sort_params.c

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -24,95 +24,51 @@ int	ft_strlen(char *str)
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int				aux;
-	unsigned int	result;
+	int	index;
 
-	aux = 0;
-	result = 0;
-	while (s1[aux] != '\0' || s2[aux] != '\0')
-	{
-		if (s1[aux] == '\0')
-			return (-((unsigned int)(s2[aux])));
-		if (s2[aux] == '\0')
-			return ((unsigned int)s1[aux]);
-		if (s1[aux] != s2[aux])
-			return (s1[aux] - s2[aux]);
-		result = s1[aux] - s2[aux];
-		s1++;
-		s2++;
-	}
-	return (result);
+	index = 0;
+	while (s1[index] && s2[index] && s1[index] == s2[index])
+		index++;
+	return (s1[index] - s2[index]);
 }
 
-void	ft_swap(char *a, char *b)
+void	ft_sort_params(char *argv[], int argc)
 {
-	char	aux;
+	char	*swap;
+	int		index;
+	int		index2;
 
-	aux = *a;
-	*a = *b;
-	*b = aux;
-}
-
-void	ft_sort_params(char *str, int size)
-{
-	int	index1;
-	int	index2;
-
-	index1 = 0;
-	while (index1 < (size - 1))
+	index = 1;
+	while (index < argc - 1)
 	{
-		index2 = 0;
-		while (index2 < (size - index1 - 1))
+		index2 = 1;
+		while (index2 < argc - index)
 		{
-			if (str[index2] > str[index2 + 1])
-				ft_swap(&str[index2], &str[index2 + 1]);
+			if (ft_strcmp(argv[index2], argv[index2 + 1]) > 0)
+			{
+				swap = argv[index2];
+				argv[index2] = argv[index2 + 1];
+				argv[index2 + 1] = swap;
+			}
 			index2++;
 		}
-		index1++;
-	}
-}
-
-void	ft_putstr(char *str)
-{
-	int	aux;
-
-	aux = 0;
-	while (str[aux])
-	{
-		write(1, &str[aux], 1);
-		aux++;
+		index++;
 	}
 }
 
 int	main(int argc, char *argv[])
 {
 	int	index;
-	int	index2;
 
-	index = 1;
 	if (argc > 1)
 	{
+		ft_sort_params(argv, argc);
+		index = 1;
 		while (index < argc)
 		{
-			ft_sort_params(argv[index], ft_strlen(argv[index]));
-			ft_putstr(argv[index]);
-			ft_putstr("\n");
-			++index;
-		}
-		index = 1;
-		while (index < (argc - 1))
-		{
-			index2 = 1;
-			while (index2 < (argc - index - 1))
-			{
-				if (ft_strcmp(argv[index2], argv[index2 + 1]) > 0)
-				{
-					ft_putstr(argv[index2]);
-					ft_putstr("\n");
-				}
-				index2++;
-			}
-			++index;
+			write(1, argv[index], ft_strlen(argv[index]));
+			write(1, "\n", 1);
+			index++;
 		}
 	}
 	return (0);
