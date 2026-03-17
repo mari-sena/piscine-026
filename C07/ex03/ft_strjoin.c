912c79526aeabb28116211b15b90560069cc246c
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-03-17 17:11:07 -0300

    ft_strjoin.c finished

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -10,33 +10,79 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdlib.h>
 
-char *ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
-	char	*aux;
-	int		index;
+	int	aux;
 
-	if (size == 0)
-		return (NULL);
-	aux = (char *)malloc(size);
-	index = 0;
-	while (index < size)
-	{
-		if ((*strs)[index] == '\0')
-			aux = sep;
-		aux = &(*strs)[index];
+	aux = 0;
+	while (str[aux])
 		aux++;
-		index++;
-	}
 	return (aux);
 }
 
-#include <stdio.h>
-int main(void)
+void	ft_continue(int size, char **strs, char *dest, char *sep)
 {
-    char	*strs[3] = {"gato", "cachorro", "sache"};
-    printf("%s", ft_strjoin(17, strs, "+"));
-    return (0);
+	int		index;
+	int		aux_index;
+	int		aux2_index;
+
+	index = 0;
+	aux2_index = 0;
+	while (index < size)
+	{
+		aux_index = 0;
+		while (strs[index][aux_index])
+			dest[aux2_index++] = strs[index][aux_index++];
+		if (index < size - 1)
+		{
+			aux_index = 0;
+			while (sep[aux_index])
+				dest[aux2_index++] = sep[aux_index++];
+		}
+		index++;
+	}
+	dest[aux2_index] = '\0';
 }
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*dest;
+	int		total;
+	int		index;
+
+	if (size == 0)
+	{
+		dest = (char *)malloc(sizeof(char));
+		if (!dest)
+			return (NULL);
+		dest[0] = '\0';
+		return (dest);
+	}
+	index = 0;
+	total = 0;
+	while (index < size)
+		total += ft_strlen(strs[index++]);
+	total += (size - 1) * ft_strlen(sep);
+	dest = (char *)malloc(total + 1);
+	if (!dest)
+		return (NULL);
+	ft_continue(size, strs, dest, sep);
+	return (dest);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+
+// 	char	*strs[3] = {"gato", "cachorro", "sache"};
+// 	char	*result;
+
+// 	result = ft_strjoin(3, strs, "+");
+// 	printf("%s\n", result);
+// 	free(result);
+// 	// printf("%lu", sizeof(char));
+// 	// printf("%lu", sizeof(int));
+// 	return (0);
+// }
